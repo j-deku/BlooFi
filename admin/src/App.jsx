dfa770f3482e224527fa454b17da4c423719f00a
Jeremiah Deku <jdeku573@gmail.com>
2025-02-04 15:05:51 +0000

    nice commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import Sidebar from "./components/SideBar/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import Add from "./pages/Add/Add";
 import Lists from "./pages/Lists/Lists";
 import Orders from "./pages/Orders/Orders";
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from "react-toastify";
+import Sidebar from "./components/SideBar/Sidebar";
 const App = () => {
   
   return (
